445dc9f31bd1553e932f5a133377870e65213774
Humberto Uchiyama Costa <humbertou.costa@gmail.com>
2025-03-07 19:06:04 -0300

    adds test robots for home screen ui tests

--- app/src/androidTest/java/com/ivy/home/HomeScreenTest.kt
+++ app/src/androidTest/java/com/ivy/home/HomeScreenTest.kt
@@ -1,18 +1,13 @@
 package com.ivy.home
 
-import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.performClick
 import com.ivy.common.androidtest.IvyAndroidTest
 import com.ivy.common.androidtest.test_data.saveAccountWithTransactions
 import com.ivy.common.androidtest.test_data.transactionWithTime
 import com.ivy.navigation.Navigator
-import com.ivy.navigation.destinations.main.Home
 import com.ivy.wallet.ui.RootActivity
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.runBlocking
-import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 import java.time.Instant
@@ -30,43 +25,32 @@ class HomeScreenTest: IvyAndroidTest() {
 
     @Test
     fun testSelectingDateRange() = runBlocking<Unit> {
-        val date = LocalDate.of(2023, 7, 23)
+        val date = LocalDate.of(2025, 2, 2)
         setDate(date)
 
-        val transaction1 = transactionWithTime(Instant.parse("2023-07-24T09:00:00Z")).copy(
+        val transaction1 = transactionWithTime(Instant.parse("2025-02-24T09:00:00Z")).copy(
             title = "Transaction1"
         )
-        val transaction2 = transactionWithTime(Instant.parse("2023-08-01T09:00:00Z")).copy(
+        val transaction2 = transactionWithTime(Instant.parse("2025-03-01T09:00:00Z")).copy(
             title = "Transaction2"
         )
-        val transaction3 = transactionWithTime(Instant.parse("2023-08-31T09:00:00Z")).copy(
+        val transaction3 = transactionWithTime(Instant.parse("2025-03-05T09:00:00Z")).copy(
             title = "Transaction3"
         )
         db.saveAccountWithTransactions(
             transactions = listOf(transaction1, transaction2, transaction3)
         )
 
-        composeRule.awaitIdle()
-        composeRule.runOnUiThread {
-            navigator.navigate(Home.route)
-        }
-
-        composeRule.onNodeWithText(date.month.name, ignoreCase = true).performClick()
-
-        composeRule
-            .onNodeWithText("August")
-            .assertIsDisplayed()
-            .performClick()
-        composeRule.onNodeWithText("Aug. 01").assertIsDisplayed()
-        composeRule.onNodeWithText("Aug. 31").assertIsDisplayed()
-
-        composeRule.onNodeWithText("Done").performClick()
-
-        composeRule.onNodeWithText("Upcoming").performClick()
-
-        composeRule.onNodeWithText("Transaction1").assertDoesNotExist()
-        composeRule.onNodeWithText("Transaction2").assertIsDisplayed()
-        composeRule.onNodeWithText("Transaction3").assertIsDisplayed()
+        HomeScreenRobot(composeRule)
+            .navigateTo(navigator)
+            .openDateRangeSheet(timeProvider)
+            .selectMonth("March")
+            .assertDateIsDisplayed(1, "March")
+            .assertDateIsDisplayed(31, "March")
+            .clickDone()
+            .clickUpcoming()
+            .assertTransactionDoesNotExist("Transaction1")
+            .assertTransactionIsDisplayed("Transaction2")
+            .assertTransactionIsDisplayed("Transaction3")
     }
-
 }
